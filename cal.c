55c3ac9c9417a3017d8fd9c0f2fe3d03cb6f5b15
Joe Carnuccio <joecar@socal.rr.com>
2018-09-21 10:56:29 -0700

    cal: Initial source code

---
+++ cal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+static char *dayname(int day)
+{
+	static char *name[] = {
+		"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+	};
+	return name[day];
+}
+
+static int firstday(int year)
+{
+	year--;
+	return (1 + year + year/4 - year/100 + year/400) % 7;
+}
+
+int main(int argc, char **argv)
+{
+	while (*++argv)
+		printf("%s starts on %s\n", *argv,
+			dayname(firstday(strtol(*argv, NULL, 10))));
+	return 0;
+}
